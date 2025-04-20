7839d479f2026779cd45bf948e7a809635f00a36
Lihini0202 <lihinisewmini12@gmail.com>
2025-04-20 15:00:11 +0530

    feat: add morgan request logging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
